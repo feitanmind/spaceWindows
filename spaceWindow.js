1174b7d3ca9321e9dee2ab39ccf4c0378a18538d
Adam Burski <burskiadamwork@gmail.com>
2023-06-14 22:22:57 +0200

    Dodanie zamykania okna oraz przycisku do tworzenia okna

--- spaceWindow.js
+++ spaceWindow.js
@@ -52,7 +52,7 @@ class SpaceWindow
                     <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707zm0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707zm-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707z"/>
                 </svg>
             </div>
-            <div class="spaceWindow-topBar-closeButton spaceWindow-topBar-button">
+            <div class="spaceWindow-topBar-closeButton spaceWindow-topBar-button" onclick="SpaceWindow.closeWindow(this)">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
                     <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
                 </svg>
@@ -64,6 +64,11 @@ class SpaceWindow
         `;
         newSpaceWindow.innerHTML = codeOfWindow;
         document.body.appendChild(newSpaceWindow);
+    }
+    static closeWindow(element)
+    {
+        let spaceWindow = element.parentElement.parentElement;
+        spaceWindow.setAttribute("class","spaceWindow");
     }
 
 }
